28022fe34f4f3685ecebda76cdea39f279c1eaf9
shina <rasheedshinaopeyemi@gmail.com>
2021-06-17 16:30:54 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import { createClient } from 'contentful';
+import RecipeCard from '../components/RecipeCard';
+
+const client = createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+
+export async function getStaticProps() {
+  const res = await client.getEntries({ content_type: 'recipe' });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
