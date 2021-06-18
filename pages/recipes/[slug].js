c0d94ea7289f128722be563a228dbf6c2f7b8c99
shina <rasheedshinaopeyemi@gmail.com>
2021-06-18 21:34:30 +0100

    hhhdhhh

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -46,7 +46,7 @@ export default function RecipeDetails({ recipe }) {
         <Image
           src={`https:${featuredImage.fields.file.url}`}
           width={featuredImage.fields.file.details.image.width}
-          height={featuredImage.fields.file.details.image.height}
+          height='600px'
         />
         <h2>{title}</h2>
       </div>
