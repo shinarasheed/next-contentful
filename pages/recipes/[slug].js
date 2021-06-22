bc150cc53038df85c7e55dd326ba23b31cacc2fb
shina <rasheedshinaopeyemi@gmail.com>
2021-06-22 10:27:20 +0100

    added a fallback page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,6 @@
 import { createClient } from 'contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
-import Skeleton from '../../components/Skeleton';
 
 export const getStaticPaths = async () => {
   const res = await client.getEntries({
@@ -16,7 +15,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
@@ -41,9 +40,6 @@ const client = createClient({
 export default function RecipeDetails({ recipe }) {
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
-
-  if (!recipe) return <Skeleton />;
-
   return (
     <div>
       <div className="banner">
